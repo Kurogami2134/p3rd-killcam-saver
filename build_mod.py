4dba51c5e54c8a1909b857734e203a977354ff7d
Kurogami2134 <90238011+Kurogami2134@users.noreply.github.com>
2024-11-24 22:13:11 -0300

    Updated to build as a modloader patch instead of a preloader mod

--- build_mod.py
+++ build_mod.py
@@ -1,6 +1,11 @@
 from struct import pack
-with open("bin/main", "rb") as file, open("bin/killcamsv.bin", "wb") as mod:
+with open("bin/main", "rb") as file, open("bin/hook", "rb") as hook, open("bin/0068P", "wb") as mod:
     data = file.read()
     mod.write(pack("2i", 0x99D0000, len(data)))
     mod.write(data)
+
+    data = hook.read()
+    mod.write(pack("2i", 0x09DC5A74, len(data)))
+    mod.write(data)
+
     mod.write(pack("2i", -1, 0))
--- src/results_patch.asm
+++ src/results_patch.asm
@@ -1,6 +1,5 @@
-.openfile "../base_files/0105.mwo", "../bin/0105.mwo", 0x09DB3D80
+.createfile "../bin/hook", 0x09DC5A74
 
-.org    0x09DC5A74
 j       input
 nop
 
